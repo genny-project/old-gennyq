57c01bd386a5b9186aa51e21c3c7066218143387
Adam Crow <adamcrow63@gmail.com>
2021-01-12 09:05:09 +1100

    Latest

--- quarkus-genny/runtime/src/test/java/org/acme/security/keycloak/authorization/KeycloakServer.java
+++ quarkus-genny/runtime/src/test/java/org/acme/security/keycloak/authorization/KeycloakServer.java
@@ -18,34 +18,34 @@ public class KeycloakServer implements QuarkusTestResourceLifecycleManager {
 	static public String keycloakUrl;
 	public static String KEYCLOAK_VERSION = new PropertiesReader("genny.properties").getProperty("keycloak.version","12.0.1");
 
-	GenericContainer   keycloak = null;
+	static public GenericContainer   keycloak  = new FixedHostPortGenericContainer("quay.io/keycloak/keycloak:" + KEYCLOAK_VERSION)//System.getProperty("keycloak.version"))
+            //.withFixedExposedPort(8580, 8080)
+            //.withFixedExposedPort(8543, 8443)
+    		.withExposedPorts(8080)
+            .withEnv("KEYCLOAK_USER", "admin")
+            .withEnv("KEYCLOAK_PASSWORD", "admin")
+            .withEnv("KEYCLOAK_LOGLEVEL", "debug")
+            .withEnv("KEYCLOAK_IMPORT", "/config/realm.json")
+            .dependsOn(MySqlServer.mysql)
+          //  .withEnv("DB_VENDOR", "H2")
+            .withEnv("DB_VENDOR", "mysql")
+            .withEnv("DB_ADDR", "127.0.0.1")
+            .withEnv("DB_PORT", MySqlServer.MYSQL_PORT)
+            .withEnv("DB_DATABASE", "gennydb")
+            .withEnv("DB_USER", "genny")
+            .withEnv("DB_PASSWORD", "password")
+            .withEnv("JAVA_OPTS_APPEND", "-Djava.awt.headless=true")
+            .withEnv("PREPEND_JAVA_OPTS", "-Dkeycloak.profile=preview -Dkeycloak.profile.feature.token_exchange=enabled -Dkeycloak.profile.feature.account_api=enabled")
+            .withClasspathResourceMapping("quarkus-realm.json", "/config/realm.json", BindMode.READ_ONLY)
+            .waitingFor(Wait.forHttp("/auth"))
+            .withStartupTimeout(Duration.ofMinutes(2));
+	
+
 	//public static GenericContainer   keycloak  = null;
     @Override
     public Map<String, String> start() {
     	System.out.println("Starting Keycloak test Server");
     	Map<String, String> returnCollections = new HashMap<String,String>();
-    	
-    	keycloak  = new FixedHostPortGenericContainer("quay.io/keycloak/keycloak:" + KEYCLOAK_VERSION)//System.getProperty("keycloak.version"))
-                //.withFixedExposedPort(8580, 8080)
-                //.withFixedExposedPort(8543, 8443)
-        		.withExposedPorts(8080)
-                .withEnv("KEYCLOAK_USER", "admin")
-                .withEnv("KEYCLOAK_PASSWORD", "admin")
-                .withEnv("KEYCLOAK_LOGLEVEL", "debug")
-                .withEnv("KEYCLOAK_IMPORT", "/config/realm.json")
-                .dependsOn(MySqlServer.mysql)
-                .withEnv("DB_VENDOR", "H2")
-                .withEnv("DB_VENDOR", "mysql")
-                .withEnv("DB_ADDR", "127.0.0.1")
-                .withEnv("DB_PORT", MySqlServer.MYSQL_PORT)
-                .withEnv("DB_DATABASE", "gennydb")
-                .withEnv("DB_USER", "genny")
-                .withEnv("DB_PASSWORD", "password")
-                .withEnv("JAVA_OPTS_APPEND", "-Djava.awt.headless=true")
-                .withEnv("PREPEND_JAVA_OPTS", "-Dkeycloak.profile=preview -Dkeycloak.profile.feature.token_exchange=enabled -Dkeycloak.profile.feature.account_api=enabled")
-                .withClasspathResourceMapping("quarkus-realm.json", "/config/realm.json", BindMode.READ_ONLY)
-                .waitingFor(Wait.forHttp("/auth"))
-                .withStartupTimeout(Duration.ofMinutes(2));
     	
  
         keycloak.start();
