29b28fa1a2b7db153fd43d7e3dc96f0ee57fb990
Roger Xia <rogerxiaxia@hotmail.com>
2021-01-14 16:14:34 +1100

    wip: merge messageq

---
+++ messagesq/src/main/java/life/genny/vertx/eventbus/BaseEntityUtils.java
@@ -0,0 +1,29 @@
+package life.genny.vertx.eventbus;
+
+
+
+//import life.genny.models.GennyToken;
+//import life.genny.models.entity.BaseEntity;
+
+import java.io.Serializable;
+
+public class BaseEntityUtils implements Serializable {
+
+	/**
+	 * 
+	 */
+
+//	public BaseEntity getBaseEntityByCode(final String code) {
+//		BaseEntity be = new BaseEntity();
+//		be.code ="test-1-gada";
+//		be.name = "name-1-gada";
+//
+//		return be;
+//	}
+//
+//
+//	public GennyToken getGennyToken(){
+//		GennyToken gennyToken = new GennyToken();
+//		return gennyToken;
+//	}
+}
