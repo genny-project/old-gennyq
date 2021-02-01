946bceb2c95e6bfe83550ead5d76c2c8fb9f9d46
Adam Crow <adamcrow63@gmail.com>
2021-02-01 13:20:02 +1100

    buildall.sh

---
+++ buildall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+parms=$1
+for project in qwandaq quarkus-genny 
+do
+    echo $project
+    cd $project
+    parentdir="$(dirname `pwd`)"
+    rm -Rf $parentdir/$project/target/*
+    mvn -N versions:update-child-modules -Dmaven.test.skip=true
+    mvn clean install -DskipTests=true
+    cd ..
+    #mvn $clean install 
+done
+
+
+for project in apiq bridgeq messagesq keisha genny-proxy notes shleemy
+do
+    echo $project
+    cd $project
+    parentdir="$(dirname `pwd`)"
+    rm -Rf $parentdir/$project/target/*
+    mvn -N versions:update-child-modules -Dmaven.test.skip=true
+    mvn clean package -DskipTests=true
+    ./build-docker.sh
+    cd ..
+done
