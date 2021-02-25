57b7720bad1bd7dab2052bb02d996763b044e3bc
Adam Crow <adamcrow63@gmail.com>
2021-02-25 12:44:21 +1100

    Updated more projects to 12.0.0, added jian database dev

--- messagesq/src/test/java/life/genny/vertx/eventbus/NativeChatResourceIT.java
+++
@@ -1,9 +0,0 @@
-package life.genny.vertx.eventbus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeChatResourceIT extends ChatResourceTest {
-
-    // Execute the same tests but in native mode.
-}
